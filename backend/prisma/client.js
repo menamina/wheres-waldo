3360118887efb36bef3c4c147fbfcc307cd01514
Mena <dayjacs03@gmail.com>
2026-02-06 18:40:35 -0600

    router + controls testing

--- backend/prisma/client.js
+++ backend/prisma/client.js
@@ -2,8 +2,12 @@ require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
 const { Pool } = require("pg");
+const connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DB_URL
+    : process.env.DATABASE_URL;
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,9 +1,43 @@
 const request = require("supertest");
+
+jest.mock("../prisma/client", () => ({
+  person: {
+    findUnique: jest.fn(),
+  },
+}));
+
+const prisma = require("../prisma/client");
+
 const { server } = require("./app");
 
-Test("start time returns 0", (done) => {
-  request(server)
-    .get("/start")
-    .expect("Content-Type", /json/)
-    .expect({ startTime: 0 });
+test("start time returns 0", async () => {
+  const res = await request(server).get("/start");
+  expect(res.statusCode).toBe(200);
+  expect(res.body).toEqual({ startTime: 0 });
+});
+
+test("handles click out of bounds", async () => {
+  prisma.person.findUnique.mockResolvedValue({
+    name: "Wilma",
+    xMin: 0.6,
+    xMax: 0.8,
+    yMin: 0.6,
+    yMax: 0.8,
+  });
+  const res = await request(server)
+    .post("/click")
+    .send({ x: 7, y: 7, person: "Wilma" });
+
+  expect(prisma.person.findUnique).toHaveBeenCalledWith({
+    where: {
+      name: "Wilma",
+    },
+  });
+
+  expect(res.statusCode).toBe(200);
+  expect(res.body).toEqual({ message: false });
+});
+
+test("timer stops when all characters found", (done) => {
+  request(server).patch("/stop").send({ elapsed: 50000 }).expect(200, done);
 });
